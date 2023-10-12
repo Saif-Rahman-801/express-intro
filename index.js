0b1519cb50ce4b26cf8fb6c826f64c502baa85a0
Saif-Rahman-801 <saifurs102@gmail.com>
2023-10-12 19:52:15 +0600

    POST method used

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // import express from "express";
 const express = require("express");
 const data = require("./data.json");
+const users = require("./users.json");
 const app = express();
-const port = 3000;
+// const port = 5000;
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.get("/users", (req, res) => res.send(users));
 
 app.get("/data", (req, res) => res.send(data));
 
@@ -14,6 +22,15 @@ app.get("/data/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const sData = data.find((d) => d.id === id);
   res.send(sData);
+});
+
+app.post("/users", (req, res) => {
+  console.log("Post api hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
